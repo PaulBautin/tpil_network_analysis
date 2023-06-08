d8fff43820275e7f998d7b206e64a84cdb3d9d6f
MARCANTOINE FORTIER <marc-antoine.fortier@usherbrooke.ca>
2023-06-08 17:29:23 -0400

    Added find_files_with_common_name function for concatenation of all .csv files for a group Added z_score function to make operations on group matrices (mean, std, subtract)

--- connectivity_analysis.py
+++ connectivity_analysis.py
@@ -24,6 +24,7 @@ import numpy as np
 import os
 import argparse
 import matplotlib.pyplot as plt
+import glob
 
 
 def get_parser():
@@ -60,8 +61,31 @@ def get_parser():
     )
     return parser
 
+def z_score(df_con, df_clbp):
+    df_con_mean = df_con.groupby("roi").mean()
+    print(df_con_mean)
+    
+    df_con_std = df_con.groupby("roi").std()
+    df_clbp = df_clbp.set_index(["roi"])
+    df_abnor = df_clbp.subtract(df_con_mean, level="participant_id")
+    print(df_abnor)
+    df_abnor_mean = df_abnor.groupby("roi").mean()
+    # for clbp in df_clbp:
+    #     z = (clbp - np.mean(df_con)) / np.std(df_con)
+    # print(np.mean(z))
 
 
+def find_files_with_common_name(directory, common_name):
+    file_paths = glob.glob(directory + '/*/Compute_Connectivity/' + common_name)
+    n = range(len(file_paths))
+    
+    dict_paths = {os.path.basename(os.path.dirname(os.path.dirname(file_paths[i]))) : pd.read_csv(file_paths[i], header=None) for i in n}
+    df_paths = pd.concat(dict_paths)
+    df_paths = df_paths.reset_index().rename(columns={'level_0': 'participant_id', 'level_1': 'roi'})
+    return df_paths
+    
+    
+    
 def main():
     """
     main function, gather stats and call plots
@@ -73,13 +97,18 @@ def main():
     path_results_clbp = os.path.abspath(os.path.expanduser(arguments.clbp))
     path_output = os.path.abspath(arguments.o)
 
-    matrices_clbp = pd.read_csv(path_results_clbp, header=None)
-    matrices_con = pd.read_csv(path_results_con, header=None)
+    df_con = find_files_with_common_name(path_results_con, "commit2_weights.csv")
+    df_clbp = find_files_with_common_name(path_results_clbp, "commit2_weights.csv")
+
+    z_score(df_con, df_clbp) 
+    
 
 
-    plt.imshow(matrices_con, cmap='bwr')
-    plt.show()
 
+    #plt.imshow(matrices_con, cmap='bwr')
+    #plt.show()
+    #print(matrices_clbp)
 
 if __name__ == "__main__":
     main()
+
