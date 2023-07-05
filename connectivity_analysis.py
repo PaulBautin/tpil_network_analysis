f31c9757bb0d2defd9f8804654404b42d62d2b8f
MARCANTOINE FORTIER <marc-antoine.fortier@usherbrooke.ca>
2023-07-05 15:50:25 -0400

    Added Scilpy filters

--- connectivity_analysis.py
+++ connectivity_analysis.py
@@ -68,53 +68,36 @@ def get_parser():
     return parser
 
 def mean_matrix(df_connectivity_matrix):
-    mean_matrix = df_connectivity_matrix.groupby("roi").mean()
+    mean_matrix = df_connectivity_matrix.groupby("roi").mean() # returns mean value of all subjects for an edge
     return mean_matrix
 
 def z_score(df_con_v1, df_clbp_v1):
     df_con_mean = mean_matrix(df_con_v1)
     df_con_std = df_con_v1.groupby("roi").std()
-    df_clbp = df_clbp_v1.set_index(["subject","roi"])
+    df_clbp = df_clbp_v1.set_index(["subject","roi"]) # step necessary to apply z-score equation on every subject
     df_anor = (df_clbp - df_con_mean) / df_con_std
     df_anor_mean = mean_matrix(df_anor)
     return df_anor_mean
 
-#def graph_matrix(df_connectivity_matrix):
-    G = nx.Graph(df_connectivity_matrix)
-    num_nodes = df_connectivity_matrix.shape[0]
-    numbers = [f"{i+1:03d}" for i in range(num_nodes)]
-    names = []
-    with open('/home/mafor/dev_tpil/tpil_network_analysis/data/Brainnetome atlas.txt', 'r') as fp:
-        for line in fp:
-            x = line[:-1]
-            names.append(x)
-    
-    nx.set_node_attributes(G, names, 'label')
-    pos = nx.circular_layout(G)
-    graph = nx.draw_networkx(G, pos=pos, with_labels=True, node_color='skyblue', node_size=50, font_size=10, edge_color='gray')
-    #node_label_pos = {k: (v[0], v[1] - 0.1) for k, v in pos.items()}  # Adjust label position
-    #nx.draw_networkx_labels(G, node_label_pos, labels=names)
-    return graph
-
-def binary_mask(df_connectivity_matrix):
+def binary_mask(df_connectivity_matrix): # filters out for desired metrics
     df_abs_matrix = np.abs(df_connectivity_matrix)
-    df_binary_matrix = (df_abs_matrix > 0).astype(np.int_)
+    df_binary_matrix = (df_abs_matrix > 10.544).astype(np.int_) # threshold application (hard-coded)
     #np.where(df_abs_matrix > threshold, upper, lower)
-    df_upper_binary_matrix = np.triu(df_binary_matrix)
+    df_upper_binary_matrix = np.triu(df_binary_matrix) # keep only upper triangle of matrix to avoid duplication of data
     return df_upper_binary_matrix
 
 
 def circle(df_connectivity_matrix):
-    A = df_connectivity_matrix.values  # Convert DataFrame to NumPy array
-    N = A.shape[0]
+    A = df_connectivity_matrix.values # convert DataFrame to NumPy array
+    N = A.shape[0] #length of matrix
     # x/y coordinates of nodes in a circular layout
     r = 1
     theta = np.linspace(0, 2 * np.pi, N, endpoint=False)
     xy = np.column_stack((r * np.cos(theta), r * np.sin(theta)))
-    # labels of nodes
+    # labels of nodes with Brainnetome atlas
     numbers = [f"{i+1:03d}" for i in range(N)]
     names = []
-    with open('/home/mafor/dev_tpil/tpil_network_analysis/data/Brainnetome atlas.txt', 'r') as fp:
+    with open('/home/mafor/dev_tpil/tpil_network_analysis/data/Brainnetome atlas.txt', 'r') as fp: 
         for line in fp:
             x = line[:-1]
             names.append(x)
@@ -126,7 +109,7 @@ def circle(df_connectivity_matrix):
     line_plot = plt.plot(xy[:, 0], xy[:, 1], linestyle="none", marker="o", markersize=10, color="steelblue", alpha=0.7)
     for i in range(N):
         for j in range(i + 1, N):
-            if A[i, j] != 0:  # Change condition to consider non-zero edge intensities
+            if A[i, j] != 0:  # Considers all non-zero edge intensities
                 edge_color = A[i, j]  # Use the edge intensity as the color value
                 plt.plot([xy[i, 0], xy[j, 0]], [xy[i, 1], xy[j, 1]], color=cm.viridis(edge_color), linewidth=1)
                 label_x_i = xy[i, 0] * 1.22  # Adjust label positioning for node i
@@ -172,12 +155,12 @@ def find_files_with_common_name(directory, common_name):
 def filter_no_connections(df_connectivity_matrix):
     #df_connectivity_matrix_numeric = df_connectivity_matrix.copy()
     #df_connectivity_matrix_numeric.iloc[:, 1:] = df_connectivity_matrix.iloc[:, 1:].replace(r'^\D+$', np.nan, regex=True)
-    df_connectivity_matrix.iloc[:, 1:] = df_connectivity_matrix.iloc[:, 1:].astype(float)
+    df_connectivity_matrix.iloc[:, 1:] = df_connectivity_matrix.iloc[:, 1:].astype(float) # converts in format that np can use
     df_zero_connections = []
-    df_zero_matrix = np.zeros_like(df_connectivity_matrix.iloc[:, 1:])
+    df_zero_matrix = np.zeros_like(df_connectivity_matrix.iloc[:, 1:]) 
     for row in range(df_zero_matrix.shape[0]):
         for col in range(df_zero_matrix.shape[1]):
-            if df_connectivity_matrix.iloc[row, col+1] < 1:
+            if df_connectivity_matrix.iloc[row, col+1] < 1: # all non-zero values (integers) convert to 1
                 df_zero_matrix[row, col] = 0
                 df_zero_connections.append((row, col))
             else:
@@ -187,16 +170,39 @@ def filter_no_connections(df_connectivity_matrix):
     df_mean_matrix[df_mean_matrix < 1] = 0
     return df_mean_matrix
 
+def filter_scilpy(df_connectivity_matrix):
+    mask_con = np.load('/home/mafor/dev_tpil/tpil_network_analysis/results/results_connectflow/con_mask_streamline.npy')[:-3,:-3]
+    mask_clbp = np.load('/home/mafor/dev_tpil/tpil_network_analysis/results/results_connectflow/clbp_mask_streamline.npy')[:-3,:-3]
+    mask = mask_con * mask_clbp
+    print(mask)
+    mask_data = df_connectivity_matrix * mask
+    print(mask_data)
+    return mask_data
+
 def histogram(df_connectivity_matrix):
     data = df_connectivity_matrix.values.flatten()
+    data[np.isnan(data)] = 0
     data_nonzero = data[data != 0]
-    percentiles = np.arange(0, 100, 5)
+    percentiles = np.arange(0, 100, 5) # each bin contains 5% of all data
     bin_edges = np.percentile(data_nonzero, percentiles)
     hist, bins = np.histogram(data_nonzero, bins=bin_edges)
-    plt.bar(bins[:-1], hist, width=bins[1]-bins[0])
-    plt.xlabel('Bins')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Connectivity Matrix')
+
+    # Generate a list of colors for the bars using the "Pastel1" colormap
+    num_bins = len(bins) - 1
+    colors = plt.cm.Pastel1(np.linspace(0, 1, num_bins))
+
+    # Plotting the histogram with colored bars
+    fig, ax = plt.subplots()
+    for i in range(num_bins):
+        ax.bar(bins[i], hist[i], width=bins[i+1]-bins[i], color=colors[i])
+
+    ax.set_xlabel('Bins')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Histogram of Connectivity Matrix')
+
+    # Add ticks to show bin edges
+    ax.set_xticks(bins)
+    ax.set_xticklabels([f'{bin:.2f}' for bin in bins])
     
     return hist, bins
  
@@ -223,38 +229,25 @@ def main():
     df_con_sc_v1 = df_con[df_con_sc['session'] == "v1"].drop(["subject", "session"], axis=1)
     df_clbp_sc_v1 = df_clbp[df_clbp_sc['session'] == "v1"].drop(["subject", "session"], axis=1)
     
-    df_z_score_v1 = z_score(df_con_v1, df_clbp_v1) 
-    df_z_score_v1[filter_no_connections(df_con_sc_v1) == 0] = 0
-    df_z_score_v1[filter_no_connections(df_clbp_sc_v1) == 0] = 0
+    df_con_mean = mean_matrix(df_con_v1)
+    #np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/con_mean_hist.txt', df_con_mean, fmt='%1.3f')
+    df_con_binary = binary_mask(df_con_mean)
+
+    df_clbp_mean = mean_matrix(df_clbp_v1)
+    #df_clbp_hist = histogram(df_clbp_mean)
+    #np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/clbp_filter.txt', df_clbp_mean, fmt='%1.3f')
+    #df_clbp_binary = binary_mask(df_clbp_mean)
+    
+    df_z_score_v1 = z_score(df_con_v1, df_clbp_v1)
+    df_z_score_v1 = filter_scilpy(df_z_score_v1) 
+    #df_z_score_v1[df_clbp_binary == 0] = 0
     #df_z_score_hist = histogram(df_z_score_v1)
-    #print(df_z_score_hist)
-    #np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/z_score.csv', df_z_score_v1_binary, fmt='%1.3f')
+    np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/z_filter.csv', df_z_score_v1, fmt='%1.3f')
     #df_graph_z_score_v1 = circle(df_z_score_v1)
     #plt.show()
 
     #plt.imshow(df_z_score_v1, cmap='bwr', norm = colors.TwoSlopeNorm(vmin=-2, vcenter=0, vmax=20))
     #plt.colorbar()
-    #plt.show()
-    
-    df_con_mean = mean_matrix(df_con_v1)
-    df_con_mean[filter_no_connections(df_con_sc_v1) == 0] = 0
-    df_con_mean[filter_no_connections(df_clbp_sc_v1) == 0] = 0
-    df_con_hist = histogram(df_con_mean)
-    print(df_con_hist)
-    #np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/con_mean_hist.txt', df_con_mean, fmt='%1.3f')
-    df_con_binary = binary_mask(df_con_mean)
-    #df_con_graph = circle(df_con_binary)
-    #plt.show()
-
-    df_clbp_mean = mean_matrix(df_clbp_v1)
-    df_clbp_mean[filter_no_connections(df_clbp_sc_v1) == 0] = 0
-    df_clbp_mean[filter_no_connections(df_clbp_sc_v1) == 0] = 0
-    df_clbp_hist = histogram(df_clbp_mean)
-    print(df_clbp_hist)
-    #plt.show()
-    #np.savetxt('/home/mafor/dev_tpil/tpil_network_analysis/data/clbp_mean_hist.txt', df_clbp_mean, fmt='%1.3f')
-    df_clbp_binary = binary_mask(df_clbp_mean)
-    #df_graph_clbp = circle(df_clbp_binary)
     #plt.show()
     
 if __name__ == "__main__":
