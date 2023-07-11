762ba7bd0fdee0280ad641b780d4930ce9dfe0e2
PaulBautin <pbautin70@gmail.com>
2023-07-11 07:52:59 -0400

    modify run_connectivity_prep_cc.sh script to match compute canada

--- connectivity_prep/run_connectivity_prep_cc.sh
+++ connectivity_prep/run_connectivity_prep_cc.sh
@@ -14,7 +14,7 @@
                                # https://docs.computecanada.ca/wiki/B%C3%A9luga/en#Node_Characteristics
 #SBATCH --mem=0                # --> 0 means you take all the memory of the node. If you think you will need
                                # all the node, you can keep 0.
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 
 #SBATCH --mail-user=paul.bautin@polymtl.ca
 #SBATCH --mail-type=BEGIN
@@ -27,9 +27,9 @@
 module load StdEnv/2020 java/14.0.2 nextflow/22.10.8 apptainer/1.1.8
 
 
-my_singularity_img='/home/pabaua/scratch/tpil_dev/data/singularity_container.sif' # or .sif
+my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/singularity_container.sif' # or .sif
 my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_network_analysis/connectivity_prep/main.nf'
-my_input_tr='/home/pabaua/scratch/tpil_dev/results/clbp/23-01-24_tractoflow_bundling/results'
+my_input_tr='/home/pabaua/scratch/tpil_dev/results/clbp/23-07-05_tractoflow_bundling/results'
 my_input_fs='/home/pabaua/scratch/tpil_dev/data/22-09-21_t1_clbp_freesurfer_output'
 
 
